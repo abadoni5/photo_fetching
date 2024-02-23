8417fdc38da6a8922b84993a146fe2e2c5382e0b
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-02-23 17:40:45 +0530

    responsiveness

--- app/Components/Card.js
+++ app/Components/Card.js
@@ -1,11 +1,12 @@
+// Card.js
 import React from 'react';
 
 const Card = ({ imageUrl, altText, index }) => {
     return (
-        <div className="max-w-xs rounded-lg overflow-hidden shadow-lg bg-gray-200 mb-4 hover:bg-teal-500 hover:text-white transition duration-300">
+        <div className="max-w-xs sm:max-w-md rounded-lg overflow-hidden shadow-lg bg-gray-200 mb-4 hover:bg-teal-500 hover:text-white transition duration-300">
             <img className="w-full" src={imageUrl} alt={altText} />
             <div className="px-4 py-4 text-center">
-                <div className="text-3xl font-semibold font-mono text-gray-500 mb-2">Dr. Sample {index}</div>
+                <div className="text-2xl font-semibold font-mono text-gray-500 mb-2">Dr. Sample {index}</div>
                 <div className="text-sm text-gray-600 mb-2 font-mono">Sample Specialization</div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,25 @@ import Card from "./Components/Card";
 
 const UserImages = () => {
   return (
-    <div className="container mx-auto">
-      <h2 className="text-3xl text-gray-700 font-mono mt-4 text-center mb-8">
-        Select Doctor
-      </h2>
-      <div className="flex flex-wrap justify-center">
-        {imageData.map((item, index) => (
-          <div key={index} className="w-1/4 p-4">
-            <Card
-              imageUrl={item.image}
-              altText={`User Image ${index}`}
-              index={index + 1}
-            />
-          </div>
-        ))}
+    <div className="container mx-auto flex justify-center">
+      <div className="max-w-screen-lg">
+        <h2 className="text-3xl text-gray-700 font-mono mt-4 text-center mb-8">
+          Select Doctor
+        </h2>
+        <div className="flex flex-wrap justify-center">
+          {imageData.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-wrap justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 mx-auto"
+            >
+              <Card
+                imageUrl={item.image}
+                altText={`User Image ${index}`}
+                index={index + 1}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
